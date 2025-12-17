3e2e6a2b32095a9e9e39f94168eadf1ef2641b7a
FlavioMontoril <flavio.montorilll@gmail.com>
2025-12-17 15:40:26 -0300

    feat: implemented verification of logged-in user is the same as the one that created the account.

--- src/core/use-case/task-use-case/delete-task-use-case.ts
+++ src/core/use-case/task-use-case/delete-task-use-case.ts
@@ -1,19 +1,18 @@
+import { UnauthorizedActionError } from "core/errors/unauthorized-action.js";
 import { InvalidOperationException } from "core/exceptions/InvalidOperationException.js";
 import { ResourceNotFoundException } from "core/exceptions/ResourceNotFoundException.js";
 import { TaskRepository } from "core/repository/contracts/task-repository.js";
 import { TaskStatus } from "core/types/task-types.js";
 
-export class DeleteTaskUseCase{
-    constructor(private readonly repository: TaskRepository){}
-    public async execute(id: string){
+export class DeleteTaskUseCase {
+    constructor(private readonly repository: TaskRepository) { }
+    public async execute(id: string, loggedUserId: string) {
         const task = await this.repository.findById(id)
 
-        if(!task){
-            throw new ResourceNotFoundException()
-        }
-        if(task.getStatus() === TaskStatus.DONE){
-            throw new InvalidOperationException()
-        }
+        if (!task) throw new ResourceNotFoundException()
+        if (task.getUserId() !== loggedUserId) throw new UnauthorizedActionError()
+        if (task.getStatus() === TaskStatus.DONE) throw new InvalidOperationException()
+            
         await this.repository.delete(id)
     }
 }
--- src/infra/controller/task-controller.ts/delete-task-controller.ts
+++ src/infra/controller/task-controller.ts/delete-task-controller.ts
@@ -1,19 +1,25 @@
 import { PrismaTaskRepository } from "adapters/database/prisma/task/prisma-task-repository.js"
+import { UnauthorizedActionError } from "core/errors/unauthorized-action.js"
 import { InvalidOperationException } from "core/exceptions/InvalidOperationException.js"
 import { ResourceNotFoundException } from "core/exceptions/ResourceNotFoundException.js"
 import { MakeDeleteTaskUseCaseFactory } from "core/factory/task-factory/make-delete-task-use-case-factory.js"
 import {Request, Response} from "express"
+import { CustomRequest } from "infra/http/express/types/custom-request.js"
 import z from "zod"
 class DeleteTaskController{
-    public async handle(req: Request, res: Response){
+    public async handle(req: CustomRequest, res: Response){
         try{
             const paramsSchema = z.object({
+                id: z.string(),
+            });
+            const userSchema = z.object({
                 id: z.string(),
             })
 
             const {id} = paramsSchema.parse(req.params)
+            const {id: loggedUserId} = userSchema.parse(req.user)
             const useCase = MakeDeleteTaskUseCaseFactory.build()
-            await useCase.execute(id)
+            await useCase.execute(id, loggedUserId)
             res.sendStatus(200).json({message: "Task delete"})
         }catch(error){
             if(error instanceof z.ZodError){
@@ -27,6 +33,9 @@ class DeleteTaskController{
              if(error instanceof InvalidOperationException){
                 res.status(400).json({error: error.message})
                 return
+            }
+            if(error instanceof UnauthorizedActionError){
+                return res.status(403).json({message: error.message})
             }
              res.status(500).json({messahe: 'Internal Server Error'})
                 return
